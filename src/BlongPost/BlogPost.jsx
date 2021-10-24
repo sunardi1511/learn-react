e8a618804cad655951b2c40e644cf47a752209ce
hariyanto17 <hariyanto17@mhs.akba.ac.id>
2021-10-24 20:30:26 +0800

    API delet

--- src/BlongPost/BlogPost.jsx
+++ src/BlongPost/BlogPost.jsx
@@ -1,35 +1,45 @@
 import React, { Component } from "react";
 import "./BlogPost.css";
 import Post from "../Post/Post";
-import axios from 'axios';
+import axios from "axios";
 
 class BlogPost extends Component {
   state = {
     post: [],
   };
 
+  getPosAPI = () => {
+    axios.get("http://localhost:3004/posts").then((res) => {
+      this.setState({
+        post: res.data,
+      });
+    });
+  };
+
+  handleRemove = (data) => {
+    axios.delete(`http://localhost:3004/posts/${data}`).then((res) => {
+      this.getPosAPI()
+    });
+  };
+
   componentDidMount() {
-    // fetch("https://jsonplaceholder.typicode.com/posts")
-    //   .then((response) => response.json())
-    //   .then((json) => {
-    //     this.setState({
-    //       post: json,
-    //     });
-    //   });
-    axios.get("https://jsonplaceholder.typicode.com/posts")
-    .then((res)=>{
-        this.setState({
-            post: res.data
-        })
-    } )
+    this.getPosAPI();
   }
 
   render() {
     return (
       <>
         <p className="section-title">Blog Post</p>
-        {this.state.post.map((post) => {
-          return <Post title={post.title} desc={post.body} />;
+        <div className="form-add-post">
+          <label htmlFor="title">Title</label>
+          <input type="text" name="title" placeholder="add title"/>
+          <label htmlFor="body">blog content</label>
+          <textarea nama="body" id="body" cols="30" rows="10" placeholder="add body"></textarea>
+          <button className="btn-submit">simpan</button>
+        </div>
+        {
+          this.state.post.map((pos) => {
+            return <Post key={pos.id} data={pos} remove={this.handleRemove} />;
         })}
       </>
     );
--- src/Post/Post.jsx
+++ src/Post/Post.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const Post = ({title,desc}) =>{
+const Post = ({data,remove}) =>{
     return (
         <div className="post">
         <div className="img-thumb">
           <img src="https://placeimg.com/200/150/animals" alt="" />
         </div>
         <div className="content">
-          <p className="title">{title}</p>
-          <p className="desc">{desc}</p>
+          <p className="title">{data.title}</p>
+          <p className="desc">{data.body}</p>
+          <button className='remove' onClick={() => remove(data.id)}>remove</button>
         </div>
       </div>
     )
